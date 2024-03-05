1eee02608a6a03fbd897c6e00b22bc83cff9a4e9
Potato-Cracker <maheswarbora2@gmail.com>
2024-03-05 21:07:27 +0530

    remove second relative import

--- bert_pytorch/trainer/pretrain.py
+++ bert_pytorch/trainer/pretrain.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 
-from ..model import BERTLM, BERT, BERTLM_Dual
-from .optim_schedule import ScheduledOptim
+from model import BERTLM, BERT, BERTLM_Dual
+from optim_schedule import ScheduledOptim
 
 import tqdm
 
